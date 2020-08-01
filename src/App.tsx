7336a0f5c3b93ec271247bd6ebc26637823264f0
Christopher David <chris@arcade.city>
2020-08-01 13:26:35 -0500

    Configure axios; make first wallets API call; show master wallet ID

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Circle } from './circle'
 
 const circle = new Circle()
 
 function App() {
+    const [masterWalletId, setMasterWalletId] = useState('')
+
+    const fetchInitialData = async () => {
+        setMasterWalletId(await circle.fetchMasterWalletId())
+    }
+
     useEffect(() => {
-        circle.fetchMasterWallet()
+        fetchInitialData()
     }, [])
-    return <div className="App">Yo</div>
+
+    return <p>{`Master wallet ID: ${masterWalletId}`}</p>
 }
 
 export default App
--- src/circle.ts
+++ src/circle.ts
@@ -1,10 +1,30 @@
-// Our Circle service
+import walletsApi from './lib/walletsApi'
 
+// Our Circle service
 export class Circle {
+    apiKey: string
+
     constructor() {
-        console.log('Hello Circle')
+        this.apiKey =
+            'QVBJX0tFWTo2YjZmM2E5MTE2NGM3ZDgwNzgzMzA2YmUxNzJiOTlkNjozYjIzZmNjNzM4ZmQ3YzU5NDZmN2QzM2RhNGUyZmM0Zg=='
+        this.configureAxios()
     }
-    async fetchMasterWallet() {
-        console.log("Let's fetch master wallet")
+
+    async configureAxios() {
+        const walletsInstance = walletsApi.getInstance()
+        walletsInstance.interceptors.request.use((config) => {
+            config.headers = {
+                Authorization: `Bearer ${this.apiKey}`,
+            }
+            return config
+        })
+    }
+
+    async fetchMasterWalletId() {
+        // Grab the master wallet ID
+        const {
+            payments: { masterWalletId },
+        }: any = await walletsApi.getMasterWallet()
+        return masterWalletId
     }
 }
