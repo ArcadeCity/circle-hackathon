d7d47ab704fc12d70705eabe64170b7c82298d96
Christopher David <chris@arcade.city>
2020-08-01 13:52:34 -0500

    Format balance

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,30 @@ const circle = new Circle()
 
 function App() {
     const [masterWalletId, setMasterWalletId] = useState('')
+    const [balance, setBalance] = useState()
 
     const fetchInitialData = async () => {
         setMasterWalletId(await circle.fetchMasterWalletId())
+        setBalance(await circle.fetchBalance())
     }
 
     useEffect(() => {
         fetchInitialData()
     }, [])
 
-    return <p>{`Master wallet ID: ${masterWalletId}`}</p>
+    return (
+        <>
+            <p>{`Master wallet ID: ${masterWalletId}`}</p>
+            <p>{`Total balance (available): ${circle.formatBalance(
+                balance,
+                'available',
+            )}`}</p>
+            <p>{`Total balance (unsettled): ${circle.formatBalance(
+                balance,
+                'unsettled',
+            )}`}</p>
+        </>
+    )
 }
 
 export default App
--- src/circle.ts
+++ src/circle.ts
@@ -1,3 +1,4 @@
+import paymentsApi from './lib/paymentsApi'
 import walletsApi from './lib/walletsApi'
 
 // Our Circle service
@@ -10,21 +11,42 @@ export class Circle {
         this.configureAxios()
     }
 
-    async configureAxios() {
-        const walletsInstance = walletsApi.getInstance()
-        walletsInstance.interceptors.request.use((config) => {
-            config.headers = {
-                Authorization: `Bearer ${this.apiKey}`,
-            }
-            return config
-        })
-    }
-
     async fetchMasterWalletId() {
         // Grab the master wallet ID
         const {
             payments: { masterWalletId },
         }: any = await walletsApi.getMasterWallet()
         return masterWalletId
+    }
+
+    async fetchBalance() {
+        const balance: any = await paymentsApi.getBalance()
+        console.log(balance)
+        return balance
+    }
+
+    formatBalance(balance: any, type: string) {
+        if (!balance) {
+            return '---'
+        } else if (type === 'available' && balance.available && balance.available[0]) {
+            return `${balance.available[0].amount} ${balance.available[0].currency}`
+        } else if (type === 'unsettled' && balance.unsettled && balance.unsettled[0]) {
+            return `${balance.unsettled[0].amount} ${balance.unsettled[0].currency}`
+        } else {
+            return '0 USD'
+        }
+    }
+
+    async configureAxios() {
+        const circleAuthInterceptor = (config: any) => {
+            config.headers = {
+                Authorization: `Bearer ${this.apiKey}`,
+            }
+            return config
+        }
+        const paymentsInstance = paymentsApi.getInstance()
+        const walletsInstance = walletsApi.getInstance()
+        paymentsInstance.interceptors.request.use(circleAuthInterceptor)
+        walletsInstance.interceptors.request.use(circleAuthInterceptor)
     }
 }
