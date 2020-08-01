a49bc340696a5fd3f6574a948f0a5cd328cbdffd
Christopher David <chris@arcade.city>
2020-08-01 13:17:05 -0500

    Start our own Circle service

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Circle } from './circle'
+
+const circle = new Circle()
 
 function App() {
+    useEffect(() => {
+        circle.fetchMasterWallet()
+    }, [])
     return <div className="App">Yo</div>
 }
 
---
+++ src/circle.ts
@@ -0,0 +1,10 @@
+// Our Circle service
+
+export class Circle {
+    constructor() {
+        console.log('Hello Circle')
+    }
+    async fetchMasterWallet() {
+        console.log("Let's fetch master wallet")
+    }
+}
